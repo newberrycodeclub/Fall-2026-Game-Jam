e8fbe3280cd97cbefdc2bbd1c0407ed8ccc382fe
ConnorLayson <clayson2573@gmail.com>
2026-09-25 15:22:26 -0400

    Added player turn in combat

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
-import json, os, sys, time
+import json, os, sys, time, random
 
 def load_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
-def combat(file):
-    with open(f"combat/{file}", "r") as f:
-        combat_data = json.laod(f)
+def combat(en_file, pl_file):
+    with open(f"combat/{en_file}", "r") as f:
+        combat_data = json.load(f)
+    with open(f"combat/{en_file}", "r") as f:
+        player_data = json.load(f)
     
     while True:
-        pass
+        #PLAYER TURN
+        print(f"--== {player_data['name']} ==--")
+        print(f"HP: {player_data['hp']}")
+        print(f"ATK: {player_data['atk']}")
+        print(f"DEF: {player_data['def']}")
+        print()
+        while True:
+            player_data['def'] = 0
+            try:
+                print("What do you do?")
+                print("(1) Attack")
+                print("(2) Defend")
+                print(f"(3) Heal ({player_data['heal_cost']} resources)")
+                choice = int(input("> "))
+                break
+            except:
+                print("Must choose a valid number.")
+        if choice == 1:
+            combat_data['hp'] = combat_data['hp'] - (player_data['atk'] - combat_data['def'])
+        if choice == 2:
+            player_data['def'] += 2
+        if choice == 3:
+            global resources
+            resources -= player_data['heal_cost']
+            player_data['hp'] += random.randint(player_data['heal_amount'][0], player_data['heal_amount'][1])
+            
 
 #changes the room
 def change_room(file):
@@ -73,7 +100,7 @@ global room_data
 room_data = ""
 change_room(file)
 global resources
-resources = 50
+resources = 100
 while True:
     #Clear screen.
     for i in range(50):
