80eb03f94246b31a5cddfa6a56001e8eb2a9fae9
ConnorLayson <clayson2573@gmail.com>
2026-09-25 14:15:41 -0400

    Added my name to the credits

--- main.py
+++ main.py
@@ -33,8 +33,7 @@ def subtract_resources(amount):
 def roll_credits():
     print("""
 Thank you to these wonderful programmers:
-            John Name
-            Johnny Name
+            Connor Layson
 
 And the person who kept this story on track:
             Abby Griffin
